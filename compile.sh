ae0f051f5243c3fa890372024c00732c27841e3d
dexior <dexior11@gmail.com>
2015-11-02 21:07:28 +0100

    Dodana kompilacja całej pracy.

--- compile.sh
+++ compile.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-files=$(ls [1-9]*.md 
+files=$(ls [1-9]*.md)
 
-for file in files; do
+for file in $files; do
 	base=$(basename $file .md)
+
 	pandoc $file -o ${base}.tex
 done
+
+pdflatex praca.tex
+bibtex praca
+pdflatex praca.tex
+pdflatex praca.tex
